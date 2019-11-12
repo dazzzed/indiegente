8e02b13da6443f125c484df152d626608b8190f7
dazzzed <ptiago07@gmail.com>
2019-11-12 23:11:24 +0000

    add server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { Track } from 'ngx-audio-player';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'indiegente';
+  msaapDisplayTitle = true;
+  msaapDisplayPlayList = true;
+  msaapPageSizeOptions = [2, 4, 6];
+  msaapDisplayVolumeControls = true;
+
+  // Material Style Advance Audio Player Playlist
+  msaapPlaylist: Track[] = [
+    {
+      title: 'Audio One Title',
+      link:
+        'https://cdn-ondemand.rtp.pt/nas2.share/wavrss/at3/1205/1899373_112355-1205021849.mp3'
+    },
+    {
+      title: 'Audio Two Title',
+      link:
+        'https://cdn-ondemand.rtp.pt/nas2.share/wavrss/at3/1201/1739695_105533-1201181858.mp3'
+    },
+    {
+      title: 'Audio Three Title',
+      link: 'Link to Audio Three URL'
+    }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgxAudioPlayerModule } from 'ngx-audio-player';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, NgxAudioPlayerModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
