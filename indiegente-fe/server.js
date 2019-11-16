0a3a758b00761f1da83492150996a701b7d100fb
dazzzed <ptiago07@gmail.com>
2019-11-16 19:41:08 +0000

    start structuring

---
+++ indiegente-fe/server.js
@@ -0,0 +1,19 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const list = require('/indiegente-be/update-list');
+
+const app = express();
+
+// app.use(list);
+// Serve only the static files form the dist directory
+// Replace the '/dist/<to_your_project_name>'
+app.use(express.static(__dirname + '/dist/indiegente'));
+
+app.get('*', function(req, res) {
+  // Replace the '/dist/<to_your_project_name>/index.html'
+  res.sendFile(path.join(__dirname + '/dist/indiegente/index.html'));
+});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
