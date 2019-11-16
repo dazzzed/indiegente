5cc40e897e3f03bc4a841b265a7b12800575abad
dazzzed <ptiago07@gmail.com>
2019-11-16 18:22:39 +0000

    fix server paths

--- server.js
+++ server.js
@@ -2,19 +2,23 @@
 const express = require('express');
 const path = require('path');
 
-const list = require('./indiegente-be/update-list');
+const list = require('./indiegente/indiegente-be/update-list');
 
 const app = express();
 
 // app.use(list);
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/indiegente-fe/dist/indiegente'));
+app.use(
+  express.static(__dirname + './indiegente/indiegente-fe/dist/indiegente')
+);
 
 app.get('*', function(req, res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
   res.sendFile(
-    path.join(__dirname + '/indiegente-fe/dist/indiegente/index.html')
+    path.join(
+      __dirname + './indiegente/indiegente-fe/dist/indiegente/index.html'
+    )
   );
 });
 // Start the app by listening on the default Heroku port
