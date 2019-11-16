8ea12714930f1ede43d32ba7edb7c1b75f63adb7
dazzzed <ptiago07@gmail.com>
2019-11-16 18:18:28 +0000

    fix frontend path

--- server.js
+++ server.js
@@ -9,9 +9,7 @@ const app = express();
 // app.use(list);
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(
-  express.static(__dirname + '/indiegente-fe/dist/indiegente-fe/indiegente')
-);
+app.use(express.static(__dirname + '/indiegente-fe/dist/indiegente'));
 
 app.get('*', function(req, res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
