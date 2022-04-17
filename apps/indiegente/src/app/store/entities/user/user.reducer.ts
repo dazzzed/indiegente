578b4386aa0a80007426f8a2b69791774e02f46c
dazzzed <ptiago07@gmail.com>
2022-04-17 20:03:57 +0100

    fix: initial track

--- apps/indiegente/src/app/store/entities/user/user.reducer.ts
+++ apps/indiegente/src/app/store/entities/user/user.reducer.ts
@@ -3,7 +3,7 @@ import { setCurrentTrack, setUser } from './user.actions';
 import { User } from './user.model';
 
 export const initialState: Readonly<User> = <User>(
-  JSON.parse(localStorage.getItem('indiegente-user') || '')
+  JSON.parse(localStorage.getItem('indiegente-user') || '1')
 );
 
 export const userReducer = createReducer(
