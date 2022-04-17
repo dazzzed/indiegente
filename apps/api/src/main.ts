57f643c92a0c8721b516124c9b9919d68a91af9c
dazzzed <ptiago07@gmail.com>
2022-04-17 18:13:23 +0100

    feat: restor api prefix

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -12,11 +12,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
-  // const globalPrefix = 'api';
-  // app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3002;
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  const port = process.env.PORT || 3333;
   await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/`);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
 }
 
 bootstrap();
